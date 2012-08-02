440dc45b16b8a5320d74d2dcc99b0ad9b90134ea
jazahn <jcleveng@fas.harvard.edu>
2012-08-02 17:37:43 -0400

    added POLICY_TEMPLATES which has the boilerplate/templates

---
+++ wizard/protected/tests/fixtures/POLICY_TEMPLATES.php
@@ -0,0 +1,25 @@
+<?php
+return array(
+	
+	'policytemplate1' => array(
+		'NAME' => 'Collaboration Permitted in Written Work',
+		'IS_ACTIVE' => '1',
+		'SORT_ORDER' => '1',
+		'BODY' => "Discussion and the exchange of ideas are essential to academic work. For assignments in this course, you are encouraged to consult with your classmates on the choice of paper topics and to share sources. You may find it useful to discuss your chosen topic with your peers, particularly if you are working on the same topic as a classmate. However, you should ensure that any written work you submit for evaluation is the result of your own research and writing and that it reflects your own approach to the topic. You must also adhere to standard citation practices in this discipline and properly cite any books, articles, websites, lectures, etc. that have helped you with your work. If you received any help with your writing (feedback on drafts, etc), you must also acknowledge this assistance.",
+	),
+	'policytemplate2' => array(
+		'NAME' => 'Collaboration Permitted in Problem Sets',
+		'IS_ACTIVE' => '1',
+		'SORT_ORDER' => '2',
+		'BODY' => "Discussion and the exchange of ideas are essential to doing academic work. For assignments in this course, you are encouraged to consult with your classmates as you work on problem sets. However, after discussions with peers, make sure that you can work through the problem yourself and ensure that any answers you submit for evaluation are the result of your own efforts. In addition, you must cite any books, articles, websites, lectures, etc that have helped you with your work using appropriate citation practices. Similarly, you must list the names of students with whom you have collaborated on problem sets.",
+	),
+	'policytemplate3' => array(
+		'NAME' => 'Collaboration Prohibited',
+		'IS_ACTIVE' => '1',
+		'SORT_ORDER' => '3',
+		'BODY' => "Students should be aware that in this course collaboration of any sort on any work submitted for formal evaluation is not permitted. This means that you may not discuss your problem sets, paper assignments, exams, or any other assignments with other students. All work should be entirely your own and must use appropriate citation practices to acknowledge the use of books, articles, websites, lectures, discussions, etc., that you have consulted to complete your assignments.",
+	),
+
+);
+
+?>
