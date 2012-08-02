7a5e6c68f0cb7fa1403c643a07ee781e1896e20f
jazahn <jcleveng@fas.harvard.edu>
2012-08-02 02:47:34 -0400

    added policies fixture

---
+++ wizard/protected/tests/fixtures/POLICIES.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	
+	'policy1' => array(
+		'EXTERNAL_ID' => '1',
+		'IS_PUBLISHED' => '',
+		'PUBLISHED_BY' => 'me',
+		'PUBLISHED_DATE' => "",
+		'BODY' => "this is the body",
+	),
+);
+
+?>
