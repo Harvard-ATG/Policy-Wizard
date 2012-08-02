7d77418a36e099b8d22d79e8841efefde6b2e956
jazahn <jcleveng@fas.harvard.edu>
2012-08-02 02:24:18 -0400

    removed references to ico files

--- wizard/protected/views/layouts/isites.php
+++ wizard/protected/views/layouts/isites.php
@@ -23,11 +23,13 @@ $host = "http://".$_SERVER['HTTP_HOST'];
     <![endif]-->
 
     <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="<?php echo $host; ?>/ico/favicon.ico"/>
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo $host; ?>/ico/apple-touch-icon-144-precomposed.png"/>
+    <!--
+	<link rel="shortcut icon" href="<?php echo $host; ?>/ico/favicon.ico"/>
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo $host; ?>/ico/apple-touch-icon-144-precomposed.png"/>
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo $host; ?>/ico/apple-touch-icon-114-precomposed.png"/>
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo $host; ?>/ico/apple-touch-icon-72-precomposed.png"/>
     <link rel="apple-touch-icon-precomposed" href="<?php echo $host; ?>/ico/apple-touch-icon-57-precomposed.png"/>
+	-->
 	
 	<script>
 	topicId = "<?php echo $_REQUEST['topicId']; ?>";
