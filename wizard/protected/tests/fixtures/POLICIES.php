30f3f613902a2a4edc0892f1f671bfd38ddf867e
jazahn <jcleveng@fas.harvard.edu>
2012-08-04 00:40:25 -0400

    added another fixture that's published

--- wizard/protected/tests/fixtures/POLICIES.php
+++ wizard/protected/tests/fixtures/POLICIES.php
@@ -8,6 +8,12 @@ return array(
 		'PUBLISHED_DATE' => "",
 		'BODY' => "this is the body",
 	),
-);
+	'policy2' => array(
+		'EXTERNAL_ID' => '3',
+		'IS_PUBLISHED' => '1',
+		'PUBLISHED_BY' => 'me',
+		'PUBLISHED_DATE' => "",
+		'BODY' => "this is the body",
+	),);
 
 ?>
